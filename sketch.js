8126c013e381c86fe6a938f8a596412dc3fa7540
unknown <aserafin4@wp.pl>
2021-05-15 10:03:58 +0200

    Add possiblitiy of picking individual string

--- sketch.js
+++ sketch.js
@@ -4,9 +4,12 @@ let pitch;
 let mic;
 let cnv;
 let playing;
+let isStringSelected=false;
+let pickedNote;
 let freq = 0;
 let threshold = 1;
 let i=1;
+//tablica wybranych dźwięków
 let notes = [
     {
       note: 'E',
@@ -33,16 +36,20 @@ let notes = [
         freq: 329.63 
       }
   ];
-  
+  //odświeżanie rysowania canvasu 
   function setup() {
-    cnv=createCanvas(400, 400);
-    audioContext = getAudioContext();
-    mic = new p5.AudioIn();
-    mic.start(listening);
-    osc = new p5.Oscillator('sine');
-    cnv.mouseClicked(toggleSound)
+
+      cnv=createCanvas(400, 400);
+      audioContext = getAudioContext();
+      mic = new p5.AudioIn();
+      mic.start(listening);
+      osc = new p5.Oscillator('sine');
+      cnv.mouseClicked(toggleSound)
+    
+   
    
   }
+  //granie dźwięku na kliknięcie
   function toggleSound() {
     if(playing){
         osc.stop();
@@ -58,40 +65,75 @@ let notes = [
     pitch = ml5.pitchDetection(model_url, audioContext, mic.stream, modelLoaded);
   }
   
+  //rysuje stroik
   function draw() {
-      
+    if(isStringSelected){
+      drawTuner();
+    }
+   
+   
+
+  }
+  
+  function modelLoaded() {
+    console.log('model loaded');
+    pitch.getPitch(gotPitch);
+  }
+  
+  function gotPitch(error, frequency) {
+
+    if (error) {
+      console.error(error);
+    } else {
+
+      if (frequency) {
+        freq = frequency;
+      }
+      pitch.getPitch(gotPitch);
+    }
+  }
+
+
+
+
+
+
+
+
+function drawPickString(){
+ console.log(1);
+}
+
+
+  function drawTuner(){
     background(0);
     textAlign(CENTER, CENTER);
     fill(255);
     textSize(32);
-    text(freq.toFixed(2), width / 2, height - 150);
+    text(freq.toFixed(2)+"Hz", width / 2, height - 150);
   
-    let closestNote = -1;
+    let closestNote = notes[pickedNote];
     let recordDiff = Infinity;
-    for (let i = 0; i < notes.length; i++) {
-      let diff = freq - notes[i].freq;
+
+      let diff = freq - notes[pickedNote].freq;
       if (abs(diff) < abs(recordDiff)) {
-        closestNote = notes[i];
         recordDiff = diff;
-        
       }
-    }
-  
+      
+    
+    //Note font i położenie
     textSize(64);
+
     text(closestNote.note, width / 2, height - 50);
   
-    let diff = recordDiff;
-    // let amt = map(diff, -100, 100, 0, 1);
-    // let r = color(255, 0, 0);
-    // let g = color(0, 255, 0);
-    // let col = lerpColor(g, r, amt);
-  
+    diff = recordDiff;
+
     let alpha = map(abs(diff), 0, 120, 255, 0);
     rectMode(CENTER);
     fill(255, alpha);
     stroke(255);
     strokeWeight(1);
-
+    /// rysowanie odległości od poprawnego dźwięku (czerwony/zielony) w zależności od zmiennej diff
     if(diff>30){
         for(let i=1;i<=3;i++){
             fill(255, 0, 0);
@@ -123,7 +165,7 @@ let notes = [
             fill(255, 0, 0);
             rect(250-(i*50), 200, 50, 50);
         }
-}
+    }
     if(diff<-30){
         for(let i=1;i<=3;i++){
             fill(255, 0, 0);
@@ -135,7 +177,7 @@ let notes = [
       fill(0, 255, 0);
       
     }
-    
+    ///rysowanie osi dużej
     rect(200, 100, 200, 50);
   
     stroke(255);
@@ -147,28 +189,19 @@ let notes = [
     if (abs(diff) < threshold) {
       fill(0, 255, 0);
     }
-    rect(200 + diff / 2, 100, 10, 75);
+    if(freq<closestNote.freq/2.4){
+      rect(100, 100, 10, 75);
+    }
+    else if(freq>closestNote.freq*2.4){
+      rect(290, 100, 10, 75);
+    }
+    else{
+      rect(200 + diff / 2, 100, 10, 75);
+    }
+
     if (playing) {
         osc.freq(closestNote.freq, 0.1);
         osc.amp(1, 1);
       }
       console.log(closestNote.freq);
-  }
-  
-  function modelLoaded() {
-    console.log('model loaded');
-    pitch.getPitch(gotPitch);
-  }
-  
-  function gotPitch(error, frequency) {
-
-    if (error) {
-      console.error(error);
-    } else {
-
-      if (frequency) {
-        freq = frequency;
-      }
-      pitch.getPitch(gotPitch);
-    }
   }
